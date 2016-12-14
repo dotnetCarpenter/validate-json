0fc884c3299ae835ae86b4a417d8af467b789b80
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-12-15 00:51:18 +0100

    fix bug where errors at position 0 would couse an infinite loop

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ function validate(input, silent) {
     let valid = false
 
     try {
-
         JSON.parse(input)
         valid = true
-
     } catch(exception) {
-
         const errorPosition = /\d+$/
         const red = process.stdout.isTTY ? msg => `\u001B[31m${msg}\u001B[39m` : x=>x
 
@@ -20,7 +17,6 @@ function validate(input, silent) {
         if( errorPosition.test(exception.message) ) {
 
             const position = exception.message.match(errorPosition)[0]|0
-
 
             warn(
                 input.slice(0, position) +
@@ -46,10 +42,10 @@ function validate(input, silent) {
 
 function recalcPosition(position, input) {
     let i = 0|0, newPos = 0|0
-    do {
+    while (i !== position) {
         newPos++
-        if(input[i] === "\n") newPos = 0
+        if(input[i] === "\n") newPos = 0|0
         i++
-    } while (i !== position)
+    }
     return newPos
 }
